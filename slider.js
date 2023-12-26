7565a19166061b893e6f273f1c947331525d076d
Divyansh Jindal <ninjsa@hotmail.com>
2023-12-26 21:51:24 +0530

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,6 +1,19 @@
+ const hamburger = document.getElementById("hamburger");
+ const navlist = document.getElementById("navlist");
+
+hamburger.addEventListener("click", ()=>{
+
+  navlist.classList.toggle("navlist-active")
+
+})
+
+
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
+    // autoplay:{
+    //   Delay: 2500
+    // }
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
